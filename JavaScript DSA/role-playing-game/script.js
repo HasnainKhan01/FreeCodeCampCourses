4df2827109924f635a4c084804235c49df6da03c
Shadab <skhan36@horizon.csueastbay.edu>
2024-12-25 15:44:43 -0800

    added functions

--- JavaScript DSA/role-playing-game/script.js
+++ JavaScript DSA/role-playing-game/script.js
@@ -77,3 +77,84 @@ const locations = [
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+// Functions
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
+}
+  
+function goTown() {
+    update(locations[0]);
+}
+  
+function goStore() {
+    update(locations[1]);
+}
+  
+function goCave() {
+    update(locations[2]);
+}
+  
+function buyHealth() {
+    if (gold >= 10) {
+      gold -= 10;
+      health += 10;
+      goldText.innerText = gold;
+      healthText.innerText = health;
+    } else {
+      text.innerText = "You do not have enough gold to buy health.";
+    }
+}
+  
+function buyWeapon() {
+    if (currentWeaponIndex < weapons.length - 1) {
+      if (gold >= 30) {
+        gold -= 30;
+        currentWeaponIndex++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeaponIndex].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+      } else {
+        text.innerText = "You do not have enough gold to buy a weapon.";
+      }
+    } else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
+    }
+}
+  
+function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
+}
+  
+function fightSlime() {
+    fighting = 0;
+    goFight();
+}
+  
+function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+  
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
